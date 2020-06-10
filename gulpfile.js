5659797751d561a1c0bf48961348bc91b762b8fa
Theo <24834291+HelloTakako@users.noreply.github.com>
2020-06-09 17:06:35 -0700

    hotfix: expand width of slide menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 function style() {
   // place code for your default task here
   return (
       gulp
-        .src("./src/scss/style.scss")
+        .src('./src/scss/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .on("error", sass.logError)
+        .on('error', sass.logError)
         .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./public/css/"))
+        .pipe(gulp.dest('./public/css/'))
   );
 }
 function watch(){
-      gulp.watch("./src/scss/*.scss", style)
+      gulp.watch('./src/scss/*.scss', style)
 }
 
 exports.style = style;
